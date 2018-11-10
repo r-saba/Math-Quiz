5148df82b2f31ce64a21e2815c659db323c276e4
Ragith Sabapathipillai <ragith.coolkid@gmail.com>
2018-11-10 12:19:56 -0500

    Added select value on enter for answerboxes.

--- Math Quiz/Form1.cs
+++ Math Quiz/Form1.cs
@@ -79,5 +79,17 @@ namespace Math_Quiz
                 return true;
             else return false;
         }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            // Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
     }
 }
